ee85ac344b92c315ed3de6c8aab7ae9920ae99cd
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-25 19:48:25 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,26 @@
 class PostsController < ApplicationController
-
+#helper_method :params #to ensure that our view has access to the controllers params hash #do not place here. Reading params is a controller concern, so we don't need to expose it to the views. 
+  
+  
   def index
+   # provide a list of authors to the view for the filter control
+  @authors = Author.all
+ 
+  # filter the @posts list based on user input
+  if !params[:author].blank?
+    @posts = Post.by_author(params[:author])
+  elsif !params[:date].blank?
+    if params[:date] == "Today"
+      @posts = Post.from_today
+    else
+      @posts = Post.old_news
+    end
+  else
+    # if no filters are applied, show all posts
     @posts = Post.all
   end
+end
+  
 
   def show
     @post = Post.find(params[:id])
